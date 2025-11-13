309f25bcb026d8594c40bcd2d7fc400f4549bff6
Marten-Granath <webdant2@gmail.com>
2025-11-13 21:31:10 +0100

    server setup

---
+++ README.md
@@ -0,0 +1,20 @@
+---------------
+: Usage Guide :
+---------------
+
+This is a step-for-step instruction for how to run this program.
+
+1. Download and install GoLang at https://go.dev/doc/install
+
+2. Use the terminal and navigate from root to main folder
+
+3. Execute the following command: 
+
+```          go run .         ```
+
+4. Open the following URL in your browser:
+
+``` http://localhost:8080/monkeys ```
+
+5. Check your terminal for a fun response
+
--- main/main.go
+++ main/main.go
@@ -3,26 +3,26 @@ package main
 import (
 	"fmt"
 	"log"
-
-	"example.com/server"
+	"net/http"
 )
 
+/*
+ * ::::::::::::::::::::::
+ * :: Helper Functions ::
+ * ::::::::::::::::::::::
+ */
+
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Bananas are for monkeys, drink gainomax instead, no sponsor")
+}
+
+/*
+ * :::::::::::::::::::
+ * :: Main Function ::
+ * :::::::::::::::::::
+ */
+
 func main() {
-	// Set properties of the predefined Logger, including
-	// the log entry prefix and a flag to disable printing
-	// the time, source file, and line number.
-	log.SetPrefix("greetings: ")
-	log.SetFlags(0)
-
-	// A slice of names.
-	names := []string{"Gladys", "Samantha", "Darrin"}
-
-	// Request greeting messages for the names.
-	messages, err := server.Hellos(names)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// If no error was returned, print the returned map of
-	// messages to the console.
-	fmt.Println(messages)
+	http.HandleFunc("/", httpHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
